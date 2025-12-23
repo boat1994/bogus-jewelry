5ee1a9636a34e28e72be726b91492658e0bf37af
boat1994 <36528988+boat1994@users.noreply.github.com>
2025-12-23 23:26:44 +0700

    add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20250929_111647 from './20250929_111647';
+import * as migration_20251223_162156_add_site_settings from './20251223_162156_add_site_settings';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250929_111647.down,
     name: '20250929_111647',
   },
-]
+  {
+    up: migration_20251223_162156_add_site_settings.up,
+    down: migration_20251223_162156_add_site_settings.down,
+    name: '20251223_162156_add_site_settings'
+  },
+];
